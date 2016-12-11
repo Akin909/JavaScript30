08a7864e9c52881dc2b29578662ce8657022c540
Akin909 <akin.sowemimo@gmail.comgit config --global user.name Akin909ls git config --global user.email akin.sowemimo@gmail.com>
2016-12-11 14:21:00 +0000

    initialize repo

---
+++ drumKit/drumKit.js
@@ -0,0 +1,26 @@
+
+//create two node lists of all audo and drums then use for each to add mouseover function and play on mouseover
+let audio = document.querySelectorAll("audio")
+let drums = document.querySelectorAll("div.drums")
+
+audio.forEach(function(sound,i){
+  drums[i].onmouseover = function(){
+      sound.currentTime = 0;
+      return sound.play();
+    }
+});
+
+window.addEventListener("keydown",function(event){
+//Using keydown event object find keyCode use it to find specific audio
+  let drums = document.querySelectorAll("div.drums");
+  drums
+  let key = event.keyCode;
+  let audio = document.querySelector(`audio[data-key="${key}"]`)
+  let drum = audio.parentElement;
+  drum.className += " playing"
+  window.addEventListener("keyup",function(event){
+    drum.className = "drums"
+  })
+  audio.currentTime = 0;//set to rewind to start
+  audio.play();
+})
