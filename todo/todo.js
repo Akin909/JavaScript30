dc17823a2a3186d3abe7dd69bc08aa456c4564e8
Akin909 <akin.sowemimo@gmail.comgit config --global user.name Akin909ls git config --global user.email akin.sowemimo@gmail.com>
2016-12-18 15:44:06 +0000

    add todo completed to do files

--- todo/todo.js
+++ todo/todo.js
@@ -1,43 +1,41 @@
 
 const labels = document.querySelectorAll('label');
 const checkboxes = document.querySelectorAll('input')
-console.log(checkboxes)
 
+let lastChecked;
 
-
-
-checkboxes.forEach((checkbox, nextbox)=>{
-  checkbox.addEventListener("click", (event)=>{
-    if (checkbox.checked === true){
-      let done = checkbox
-      Array.from(checkboxes)
-      .map((checkbox)=>{
-        if(checkbox.checked === false && parseInt(checkbox.dataset.id)<done.dataset.id){
-          return checkbox.checked = true;
-        }
-      })
-    }
-  })
+function handleCheck(event){
+  let inBetween = false;
+  if(event.shiftKey && this.checked){
+    checkboxes.forEach((checkbox)=>{
+      if(checkbox === this || checkbox === lastChecked){
+        inBetween = !inBetween
+        console.log(inBetween)
+      }
+      if(inBetween){
+        checkbox.checked = true;
+      }
+    });
+  }
+  lastChecked = this;
+}
+checkboxes.forEach((checkbox)=>{
+  checkbox.addEventListener("click", handleCheck)
 })
 
-// document.addEventListener("keydown",strikeThrough);
-
-// function strikeThrough(event){
-//   if (event.keyCode === 16){
-//   labels.forEach((label)=>{
-//     label.addEventListener('keydown',(event)=>{
-//       console.log("hello they clicked a label")
-//        const checked = document.querySelectorAll('input[class="todo"]:checked')
-//        checked.forEach((checkbox)=>{
-//          console.log(parseInt(checked.dataset.id))
-//          const todo = document.querySelectorAll('input');
-        //  todo.forEach((item)=>{
-        //    if(parseInt(todo.dataset.id) < parseInt(checkbox.dataset.id)){
-        //      console.log("im less than checked box")
-        //    }
-        //  })
-//        })
-//       })
-//     })
-//   }
-// };
+  //My attempt to solve this which triggers a similar effect but on clicking shift
+  //   if (checkbox.checked === true){
+  //     let firstPressed = checkbox
+  //     Array.from(checkboxes)
+  //     .map((checkbox)=>{
+  //       window.addEventListener("keydown",(event)=>{
+  //         if(event.shiftKey){
+  //           let lastPressed = event.target
+  //           if(checkbox.checked === false && checkbox.dataset.id > firstPressed.dataset.id && checkbox.dataset.id<lastPressed.dataset.id){
+  //             console.log("lastPressed",lastPressed,"firstPressed",firstPressed)
+  //               return checkbox.checked = true;
+  //           }
+  //         }
+  //       })
+  //     })
+  //   }
