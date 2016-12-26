f8e0bd5eb9bfdc061191e1735ff62e0ece778e75
Akin909 <akin.sowemimo@gmail.comgit config --global user.name Akin909ls git config --global user.email akin.sowemimo@gmail.com>
2016-12-26 13:13:34 +0000

    add changes to save input using local storage with css changes on click

--- local-storage/local-storage.js
+++ local-storage/local-storage.js
@@ -1,8 +1,9 @@
 const addItems = document.querySelector('form.todo');
 const produce = document.querySelector('.produce');
-const items = [];
+const items = JSON.parse(localStorage.getItem('items'))|| [];
 
-addItems.addEventListener('submit',addItem);
+
+
 function addItem(event){
   event.preventDefault();
   const text = (this.querySelector('[name=item]').value)
@@ -13,8 +14,9 @@ function addItem(event){
   };
 items.push(item);
 this.reset();
-populateList(items,produce)
-console.table(items)
+populateList(items,produce);
+
+localStorage.setItem('items',JSON.stringify(items));
 }
 
 function populateList(items = [], produceList){
@@ -22,10 +24,23 @@ function populateList(items = [], produceList){
     return `
             <li>
               <input type="checkbox" data-index=${i} id="item${i}" ${item.done?'checked':''}/>
-              <label for="items${i}">
+              <label for="item${i}">
                 ${item.text}
               </label>
             </li>
             `;
   }).join('');
+};
+
+function toggleDone(event){
+
+  if(!event.target.matches('input'))return;//Skips element if it is not an input
+  const input = event.target;
+  let index = input.dataset.index;
+  items[index].done = !items[index].done;
+  localStorage.setItem('items',JSON.stringify(items));
 }
+
+populateList(items,produce)
+addItems.addEventListener('submit',addItem);
+produce.addEventListener('click',toggleDone)
